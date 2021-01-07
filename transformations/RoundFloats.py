ba93f60a915b051ffa9286f609de16343196f0e4
taniacarvalho <taniammc.94@gmail.com>
2021-01-07 12:44:45 +0000

    Add RoundFloats

---
+++ transformations/RoundFloats.py
@@ -0,0 +1,29 @@
+import warnings
+import numpy as np
+
+
+def roundFloats(obj):
+    """
+    Limit floats to two decimal points.
+    :param obj: input dataframe.
+    :return: dataframe with rounded floats.
+    """
+    return Round(obj=obj).verify_errors()
+
+
+class Round:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def verify_errors(self):
+        keyVars = self.obj.select_dtypes(include=np.float).columns
+        if len(keyVars) == 0:
+            warnings.warn("Dataframe does not have any floating point to round!")
+        else:
+            return self.roundWork(keyVars)
+
+    def roundWork(self, keyVars):
+        for col in keyVars:
+            self.obj[col] = round(self.obj[col], 2)
+
+        return self.obj
