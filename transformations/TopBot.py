b624b6395b70914358f3809da9110aa6a89e9762
taniacarvalho <taniammc.94@gmail.com>
2021-01-07 12:48:40 +0000

    Fix topBotWork issues

--- transformations/TopBot.py
+++ transformations/TopBot.py
@@ -17,24 +17,35 @@ class TopBot:
         self.keyVars = self.obj.select_dtypes(include=np.number).columns
 
     def verify_errors(self):
-        if isinstance(self.column, list):
-            raise ValueError("Length of argument 'column' > 1\n")
-        if self.column not in self.obj.columns:
-            raise ValueError("Variable specified in 'column' can not be found!\n")
-        if not is_numeric_dtype(self.obj[self.column]):
-            raise ValueError("Specified column is not numeric. topBottomCoding() can only be applied to numeric "
-                             "variables!\n")
-        elif is_numeric_dtype(self.obj[self.column]):
-            self.obj[[self.column]] = self.topBotWork()
-            return self.obj[[self.column]]
+        if len(self.keyVars) == 0:
+            warnings.warn("Dataframe does not have any variable to apply Top & Bottom!")
+        else:
+            return self.topBotWork()
 
     def topBotWork(self):
-        column_transformed = self.x.copy()
-        if self.kind == "top":
-            column_transformed.iloc[column_transformed > self.value] = self.replacement
-        else:
-            column_transformed.iloc[column_transformed < self.value] = self.replacement
-        return column_transformed
+        data_to_transform = self.obj.copy()
+        for j in range(0, len(self.keyVars)):
+            # outliers detection with Tukey's method
+            out_prob, outer_le, outer_ue = self.tukeys_method(self.keyVars[j])
+            if len(out_prob) != 0:
+                if self.obj[self.keyVars[j]].dtype == np.int:
+                    data_to_transform.loc[data_to_transform[self.keyVars[j]] <= outer_le, self.keyVars[j]] = int(outer_le)
+                    data_to_transform.loc[data_to_transform[self.keyVars[j]] >= outer_ue, self.keyVars[j]] = int(outer_ue)
+                else:
+                    data_to_transform.loc[data_to_transform[self.keyVars[j]] <= outer_le, self.keyVars[j]] = outer_le
+                    data_to_transform.loc[data_to_transform[self.keyVars[j]] >= outer_ue, self.keyVars[j]] = outer_ue
+
+        return data_to_transform
+
+    def tukeys_method(self, keyVar):
+        q1 = self.obj[keyVar].quantile(0.25)
+        q3 = self.obj[keyVar].quantile(0.75)
+        iqr = q3 - q1
+        outer_fence = 3 * iqr
+
+        # outer fence lower and upper end
+        outer_fence_le = q1 - outer_fence
+        outer_fence_ue = q3 + outer_fence
 
         outliers_prob = []
         for index, x in enumerate(self.obj[keyVar]):
@@ -42,3 +53,4 @@ class TopBot:
                 outliers_prob.append(index)
 
         return outliers_prob, outer_fence_le, outer_fence_ue
+
