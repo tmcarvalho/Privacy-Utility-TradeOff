089e82dd9fb275d48e2ca18ca5c1e0e4a2646264
taniacarvalho <taniammc.94@gmail.com>
2021-01-08 19:26:02 +0000

    Remove keyVars

--- ReIdentificationRisk/kAnon.py
+++ ReIdentificationRisk/kAnon.py
@@ -1,45 +1,30 @@
-import numpy as np
 
-
-def kAnonCalc(obj, keyVars):
+def kAnonCalc(obj):
     """
     Measure the individual risk base on the frequency of the sample.
-    :param obj: input data.
-    :param keyVars: names (or indices) of key variables.
-    :return: a list with frequency in the sample
+    :param obj: input dataframe.
+    :return: a list with frequency in the sample.
     """
-    return calcFreq(obj=obj, keyVars=keyVars).verify_errors()
+    return calcFreq(obj=obj).kAnonWork()
 
 
 class calcFreq:
-    def __init__(self, obj, keyVars):
+    def __init__(self, obj):
         self.obj = obj
-        self.keyVars = keyVars
-
-    def verify_errors(self):
-        columns = list(self.obj.columns.values)
-        error_vars = np.setdiff1d(self.keyVars, columns)
-        if len(error_vars) != 0:
-            raise ValueError("[" + '%s' % ', '.join(map(str, error_vars)) + "] specified in 'keyVars' can "
-                                                                            "not be found!\n")
-        else:
-            return self.kAnonWork()
 
     def kAnonWork(self):
-        fk = self.obj.groupby(self.keyVars)[self.keyVars[0]].transform(len)
+        keyVars = list(self.obj.columns.values)
+        fk = self.obj.groupby(keyVars)[keyVars[0]].transform(len)
         return fk
 
 
 def calc_max_risk(obj):
     """
-    Calculate the percentage of fk when is 1 in a dataframe
-    :param obj: input data
-    :return: percentage of fk when is 1
+    Calculate the percentage of fk when is 1 in a dataframe.
+    :param obj: input dataframe.
+    :return: percentage of fk when is 1.
     """
-    # exclude the target variable in order to perform the risk calculation
-    obj_val = obj[obj.columns[:-1]]
-    # list with all columns names
-    keyVars = list(obj_val.columns.values)
     # get frequency of keyVars
-    fk = kAnonCalc(obj=obj_val, keyVars=keyVars)
+    fk = kAnonCalc(obj=obj)
     return (fk == 1).sum() * 100 / len(fk)
+
