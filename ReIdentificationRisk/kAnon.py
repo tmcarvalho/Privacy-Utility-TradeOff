d3a1be0cb4ae352fcaa1d17df6f0cc475e49dc07
taniacarvalho <taniammc.94@gmail.com>
2021-01-18 14:48:13 +0000

    Return just the fk by each record

--- ReIdentificationRisk/kAnon.py
+++ ReIdentificationRisk/kAnon.py
@@ -1,9 +1,9 @@
 
 def kAnonCalc(obj):
     """
-    Measure the individual risk base on the frequency of the sample.
+    Measure the individual risk base on the frequency of the equivalence classes.
     :param obj: input dataframe.
-    :return: a list with frequency in the sample.
+    :return: frequency of each equivalence class for each observation.
     """
     return calcFreq(obj=obj).kAnonWork()
 
@@ -18,13 +18,5 @@ class calcFreq:
         return fk
 
 
-def calc_max_risk(obj):
-    """
-    Calculate the percentage of fk when is 1 in a dataframe.
-    :param obj: input dataframe.
-    :return: percentage of fk when is 1.
-    """
-    # get frequency of keyVars
-    fk = kAnonCalc(obj=obj)
-    return (fk == 1).sum() * 100 / len(fk)
+
 
