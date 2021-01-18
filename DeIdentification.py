d96b10aff902301d80906e3e444349f5133e9242
taniacarvalho <taniammc.94@gmail.com>
2021-01-18 14:51:53 +0000

    Parallel processes

--- DeIdentification.py
+++ DeIdentification.py
@@ -1,25 +1,26 @@
+import psutil
 from transformations import TopBot, GlobalRec, Suppression, RoundFloats, Noise
-from ReIdentificationRisk import kAnon, RecordLinkage
+from ReIdentificationRisk import kAnon, RecordLinkage, CalcRisk
 import numpy as np
 import itertools
 from decimal import Decimal
 import pandas as pd
 import time
+import ray
 
 
-# %% Load data
+# %% All functions to processing
 def load_data():
     dss = np.load('DS_clean.npy', allow_pickle=True)
     ds = dss.copy()
     return ds
 
 
-# %%
 def change_cols_types(df):
     """
-    Remove trailing zeros and assign as 'int' type
-    :param df: input data
-    :return: transformed data
+    Remove trailing zeros and assign as 'int' type.
+    :param df: input data.
+    :return: transformed data.
     """
     cols = df.select_dtypes(include=np.number).columns.values
     for col in cols:
@@ -31,37 +32,133 @@ def change_cols_types(df):
     return df
 
 
+def define_combs(df):
+    """
+    Define combinations of transformation techniques base on dtypes of the dataframe.
+    :param df: input dataframe
+    :return: list o combinations.
+    """
+    comb_cat = comb_float = comb_int = []
+    if len(np.where((df.dtypes == np.int) == True)[0]) != 0:
+        comb_int = ['topbot', 'globalrec', 'sup']
+    if len(np.where((df.dtypes == np.float) == True)[0]) != 0:
+        comb_float = ['topbot', 'round', 'noise']
+    if len(np.where(((df.dtypes == 'category') == True) | ((df.dtypes == 'object') == True))[0] |
+           ((df.dtypes == np.int) == True)[0]) != 0:
+        # percentage of uniques in all variables except floating points
+        uniques_per = df.select_dtypes(exclude=np.float).apply(lambda col: col.nunique() * 100 / len(df))
+        # define maximum percentage
+        uniques_max_per = uniques_per[uniques_per > 90]
+        if len(uniques_max_per) != 0:
+            comb_cat = ['sup']
+    transf_tech = list(set().union(comb_int, comb_float, comb_cat))
+    # transf_tech = ['topbot', 'globalrec', 'sup', 'round', 'noise']
+    comb = [c for i in range(len(transf_tech) + 1) for c in itertools.combinations(transf_tech, i)]
+    return comb
+
+
+def transformations(x, df_transf, c):
+    """
+    Apply transformation techniques to the dataframe.
+    :param x: combination.
+    :param df_transf: input dataframe
+    :return: transformed dataframe
+    """
+    if 'sup' in x:
+        df_transf = Suppression.suppression(df_transf)
+    if 'topbot' in x:
+        df_transf = TopBot.topBottomCoding(df_transf)
+    if 'round' in x:
+        df_transf = RoundFloats.roundFloats(df_transf)
+    if 'noise' in x:
+        df_transf = Noise.addNoise(df_transf)
+    if 'globalrec' in x:
+        c += 1
+        keyVars = comb = []
+        if c == 1:
+            keyVars, comb = GlobalRec.globalRecoding(df_transf)
+            if len(comb) != 0:
+                df_transf = GlobalRec.best_bin_size(df_transf, keyVars, comb)
+        else:
+            if len(comb) != 0:
+                df_transf = GlobalRec.best_bin_size(df_transf, keyVars, comb)
+
+    return df_transf, c
+
+
 # %%
-def deIdentification():
-    ds = load_data()
+# start Ray
+# ray.init()
+
+# @ray.remote
+def process_single_df(df):
+    # list to store the transformed dataframes
+    transformations_combs = []
+    # dataframe to store re-identification risk
     reID_risk = pd.DataFrame(columns=['initial_fk'])
+    c = 0
+    df = change_cols_types(df)
+    # keep target variable aside
+    tgt = df.iloc[:, -1]
+    # dataframe without target variable to apply transformation techniques
+    df_val = df[df.columns[:-1]]
+    # create combinations adequate to the dataframe
+    comb = define_combs(df_val)
+    fk_var = 'fk_per'
+    rl_var = 'rl_per'
+    for index, x in enumerate(comb):
+        df_transf = df_val.copy()
+        if index == 0:
+            # calculate initial re-identification risk with k-anonymity
+            reID_risk.loc[index, 'initial_fk'] = CalcRisk.calc_max_fk(df_transf)
+        else:
+            # apply transformations
+            df_transf, c = transformations(x, df_transf, c)
+            if "sup" in x:
+                # make sure that both datasets has equal dtypes to all columns
+                check_types = df_val.dtypes == df_transf.dtypes
+                idx = np.where(check_types == False)[0]
+                if len(idx) != 0:
+                    col = df_val.columns[idx[0]]
+                    df_val[col] = df_val[col].astype(str)
+                else:
+                    continue
+            # recalculate re-identification risk with k-anonymity
+            reID_risk.loc[index, fk_var] = CalcRisk.calc_max_fk(df_transf)
+            # limit record linkage with blocking!
+            max_unique = max(df_transf.nunique())
+            block_column = df_transf.columns[df_transf.nunique() == max_unique]
+            # calculate record linkage
+            reID_risk.loc[index, rl_var] = CalcRisk.calc_max_rl(df_transf, df_val, block_column[0],
+                                                                indexer="block")
 
-    transf_tech = ['topbot', 'globalrec', 'sup', 'round', 'noise']
+            # reset df_val because of dtype change due to the suppression
+            df_val = df[df.columns[:-1]]
+            # add target to the transformed dataset
+            df_transf[tgt.name] = tgt.values
+            # save all results
+            transformations_combs.append(df_transf)
 
-    comb = [c for i in range(len(transf_tech)+1) for c in itertools.combinations(transf_tech, i)]
+        print('Tech combs: ' + str(index) + '/' + str(len(comb)))
 
-    start_time = time.time()
-    for i in range(0, len(ds)):
-        ds[i] = change_cols_types(ds[i])
-        df_val = ds[i][ds[i].columns[:-1]]
-        for index, x in enumerate(comb):
-            df_transf = df_val.copy()
-            if index == 0:
-                reID_risk.loc[i, 'initial_fk'] = kAnon.calc_max_risk(df_transf)
-            if 'topbot' in x:
-                df_transf = TopBot.topBottomCoding(df_transf)
-            if 'sup' in x:
-                df_transf = Suppression.suppression(df_transf)
-            if 'round' in x:
-                df_transf = RoundFloats.roundFloats(df_transf)
-            if 'noise' in x:
-                df_transf = Noise.addNoise(df_transf)
-            if 'globalrec' in x:
-                df_transf = GlobalRec.globalRecoding(df_transf)
-
-            reID_risk.loc[index, 'fk_comb_' + str(index)] = kAnon.calc_max_risk(df_transf)
-            reID_risk.loc[index, 'rl_comb_' + str(index)] = RecordLinkage.recordLinkage(df_transf, df_val)
-            print('Tech combs: ' + str(index) + '/' + str(len(comb)))
-    end_time = time.time()
+    return transformations_combs, reID_risk, comb
 
 
+ds = load_data()
+start_time = time.time()
+result_ids = []
+# for i in range(len(ds)):
+#     result_ids.append(process_single_df.remote(ds[i]))
+
+all_risk = []
+all_combs = []
+all_transf_combs = []
+
+# for i in range(len(ds)):
+#     transf_combs, risk, combs = ray.get(result_ids[i])
+#     all_transf_combs.append(transf_combs)
+#     all_risk.append(risk)
+#     all_combs.append(combs)
+
+# close Ray
+# ray.shutdown()
--- Test.py
+++ Test.py
@@ -1,70 +1,123 @@
-from transformations import TopBot, GlobalRec, Suppression, RoundFloats, Noise
-from ReIdentificationRisk import kAnon, RecordLinkage
-import DeIdentification
 import itertools
+import psutil
+from ReIdentificationRisk import CalcRisk
+import DeIdentification
 import pandas as pd
 import time
-import random
-import warnings
 import numpy as np
+import ray
 
 # %%
-ds = DeIdentification.load_data()
-ds = random.choices(ds, k=10)
+ds = DeIdentification.load_data()  # 90 datasets
+# ds = random.choices(ds, k=3)
 
-# df = ds[12].copy()
+df = ds[2].copy()
 
-transf_tech = ['topbot', 'globalrec', 'sup', 'round', 'noise']
-comb = [c for i in range(len(transf_tech) + 1) for c in itertools.combinations(transf_tech, i)]
+# start Ray
+# num_cpus = psutil.cpu_count(logical=False)
+# ray.init(num_cpus=num_cpus)
+ray.init()
 
-reID_risk = pd.DataFrame(columns=['initial_fk'])
+dfs = []
+dfs.append(ds[17])
+# dfs = ds[0:10]
+df = ds[15].copy()
+# [0, 7, 9, 11, 15, 17, 21, 22, 23, 28, 29, 30, 31, 34, 35, 37, 38]
+
+# test ranges
+all_ranges = []
+magnitude = [0, 1, 2]
+for j in range(0, 12):
+    all_ranges.append(magnitude)
+comb = [i for i in itertools.product(*all_ranges)]
 
 
-def transformations(x, df_transf):
-    if 'topbot' in x:
-        df_transf = TopBot.topBottomCoding(df_transf)
-    if 'sup' in x:
-        df_transf = Suppression.suppression(df_transf)
-    if 'round' in x:
-        df_transf = RoundFloats.roundFloats(df_transf)
-    if 'noise' in x:
-        df_transf = Noise.addNoise(df_transf)
-    if 'globalrec' in x:
-        df_transf = GlobalRec.globalRecoding(df_transf)
-    return df_transf
-
-
-start_time = time.time()
-
-for i in range(0, len(ds)):
-    ds[i] = DeIdentification.change_cols_types(ds[i])
-    df_val = ds[i][ds[i].columns[:-1]]
-    fk_var = 'fk_per_' + str(i)
-    rl_var = 'rl_per_' + str(i)
+@ray.remote
+def process_single_df(df):
+    # list to store the transformed dataframes
+    transformations_combs = []
+    # dataframe to store re-identification risk
+    reID_risk = pd.DataFrame(columns=['initial_fk'])
+    df = DeIdentification.change_cols_types(df)
+    # keep target variable aside
+    tgt = df.iloc[:, -1]
+    # dataframe without target variable to apply transformation techniques
+    df_val = df[df.columns[:-1]]
+    # create combinations adequate to the dataframe
+    comb = DeIdentification.define_combs(df_val)
+    fk_var = 'fk_per'
+    rl_var = 'rl_per'
+    c = 0
     for index, x in enumerate(comb):
         df_transf = df_val.copy()
         if index == 0:
-            reID_risk.loc[i, 'initial_fk'] = kAnon.calc_max_risk(df_transf)
-        df_transf = transformations(x, df_transf)
-        max_unique = max(df_transf.nunique())
-        block_column = df_transf.columns[df_transf.nunique() == max_unique]
-        reID_risk.loc[index, fk_var] = kAnon.calc_max_risk(df_transf)
-        reID_risk.loc[index, rl_var] = RecordLinkage.calcRL(df_transf, df_val, block_column[0])
+            # calculate initial re-identification risk with k-anonymity
+            reID_risk.loc[index, 'initial_fk'] = CalcRisk.calc_max_fk(df_transf)
+        else:
+            # apply transformations
+            df_transf, c = DeIdentification.transformations(x, df_transf, c)
+            if "sup" in x:
+                # make sure that both datasets has equal dtypes to all columns
+                check_types = df_val.dtypes == df_transf.dtypes
+                idx = np.where(check_types == False)[0]
+                if len(idx) != 0:
+                    col = df_val.columns[idx[0]]
+                    df_val[col] = df_val[col].astype(str)
+                else:
+                    continue
+            # recalculate re-identification risk with k-anonymity
+            reID_risk.loc[index, fk_var] = CalcRisk.calc_max_fk(df_transf)
+            # record linkage with full indexer in cases where only exist dtype floats
+            # if all(df_transf.dtypes == np.float):
+            #     reID_risk.loc[index, rl_var] = CalcRisk.calc_max_rl(df_transf, df_val, block="",
+            #                                                         indexer="full")
+
+            # else:
+            # limit record linkage with blocking! Block column shoudn't be float dtype
+            # max_unique = max(df_transf.select_dtypes(exclude=np.float).nunique())
+            max_unique = max(df_transf.nunique())
+            block_column = df_transf.columns[df_transf.nunique() == max_unique]
+            # calculate record linkage
+            reID_risk.loc[index, rl_var] = CalcRisk.calc_max_rl(df_transf, df_val, block_column[0],
+                                                                indexer="block")
+
+            # reset df_val because of dtype change due to the suppression
+            df_val = df[df.columns[:-1]]
+            # add target to the transformed dataset
+            df_transf[tgt.name] = tgt.values
+            # save all results
+            transformations_combs.append(df_transf)
+
         print('Tech combs: ' + str(index) + '/' + str(len(comb)))
 
-    if reID_risk[rl_var].min() == 100:
-        warnings.warn("Dataframe is at max risk by record linkage!")
-    elif reID_risk[rl_var].min() == 0:
-        warnings.warn("Dataframe does not have observations with max risk by record linkage!")
-    idx_min = np.argmin(reID_risk[rl_var].values)
-    comb_idx = comb[idx_min]
-    ds[i] = transformations(comb_idx, df_val)
-    print(i)
+    return transformations_combs, reID_risk, comb
+
+
+start_time = time.time()
+result_ids = []
+for i in range(len(dfs)):
+    result_ids.append(process_single_df.remote(dfs[i]))
 
 end_time = time.time()
+total_time = end_time - start_time  # 6 datasets -> 18.83 min
 
-total_time = end_time-start_time
+all_risk = []
+all_combs = []
+all_transf_combs = []
 
-df_orig = ds[i][ds[i].columns[:-1]]
+for i in range(len(dfs)):
+    # aggregate all of the results
+    transf_combs, risk, combs = ray.get(result_ids[i])
+    all_transf_combs.append(transf_combs)
+    all_risk.append(risk)
+    all_combs.append(combs)
 
-y = transformations(x, df_val)
+# close Ray
+ray.shutdown()
+
+all_transf_combs[4][4]
+all_combs[4]
+
+# 7min -> 0
+# 2min -> 1
+
