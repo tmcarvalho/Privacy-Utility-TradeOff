3daab0efd3c5904617f9994c60a63a5bbd9e705f
tmcarvalho <taniammc.94@gmail.com>
2021-05-16 16:54:46 +0100

    Concatenate baseline results

--- Modeling/ConcatPerformance.py
+++ Modeling/ConcatPerformance.py
@@ -4,16 +4,16 @@ from os import walk
 import numpy as np
 import pandas as pd
 
-master_folder = 'Data/Final_results/AlloutputFiles'
-_, folders, _ = next(walk(master_folder))
+master_folder_transf = 'Data/Final_results/AlloutputFiles'
+_, folders_transf, _ = next(walk(master_folder_transf))
 
 # split files into original and transformed folders
-for folder in folders:
-    _, _, files = next(walk(f'{master_folder}/{folder}'))
+for folder in folders_transf:
+    _, _, files = next(walk(f'{master_folder_transf}/{folder}'))
     if "_" not in folder:
-        shutil.move(os.path.join(f'{master_folder}/{folder}'), f'{master_folder}/Originals')
+        shutil.move(os.path.join(f'{master_folder_transf}/{folder}'), f'{master_folder_transf}/Originals')
     else:
-        shutil.move(os.path.join(f'{master_folder}/{folder}'), f'{master_folder}/Transformed')
+        shutil.move(os.path.join(f'{master_folder_transf}/{folder}'), f'{master_folder_transf}/Transformed')
 
 # percentage difference
 orig_folder = 'Data/Final_results/AlloutputFiles/Originals'
@@ -22,7 +22,7 @@ _, org_folders, _ = next(walk(orig_folder))
 _, transf_folders, _ = next(walk(transf_folder))
 
 
-def concatenate(master_folder, folder):
+def concatenate(flag, master_folder, folder):
     # concatenate all 5 repetitions
     five_iter_npy_lst = []
     five_iter_rf_lst = []
@@ -55,7 +55,15 @@ def concatenate(master_folder, folder):
     logr_avg = pd.concat([df for df in five_iter_logr_lst])
     nn_avg = pd.concat([df for df in five_iter_nn_lst])
 
-    return rf_avg, bag_avg, xgb_avg, logr_avg, nn_avg
+    if flag == 0:
+        rf_avg.to_csv(f'{transf_folder}/{t_folder}/{t_folder}_rf_avg.csv', sep='\t', index=False)
+        bag_avg.to_csv(f'{transf_folder}/{t_folder}/{t_folder}_bag_avg.csv', sep='\t', index=False)
+        xgb_avg.to_csv(f'{transf_folder}/{t_folder}/{t_folder}_xgb_avg.csv', sep='\t', index=False)
+        logr_avg.to_csv(f'{transf_folder}/{t_folder}/{t_folder}_logr_avg.csv', sep='\t', index=False)
+        nn_avg.to_csv(f'{transf_folder}/{t_folder}/{t_folder}_nn_avg.csv', sep='\t', index=False)
+
+    else:
+        return rf_avg, bag_avg, xgb_avg, logr_avg, nn_avg
 
 
 # split files into original and transformed folders
@@ -63,10 +71,10 @@ metrics = ['gmean', 'acc', 'bal_acc', 'f1', 'f1_weighted']
 for t_folder in transf_folders:
     for o_folder in org_folders:
         if str(o_folder + '_') in t_folder:
-            o_rf, o_bag, o_xgb, o_logr, o_nn = concatenate(orig_folder, o_folder)
-            t_rf, t_bag, t_xgb, t_logr, t_nn = concatenate(transf_folder, t_folder)
+            o_rf, o_bag, o_xgb, o_logr, o_nn = concatenate(1, orig_folder, o_folder)
+            t_rf, t_bag, t_xgb, t_logr, t_nn = concatenate(1, transf_folder, t_folder)
 
-            # for each CV
+            # for each CV calculate the percentage difference
             for metric in metrics:
                 # 100 * (Sc - Sb) / Sb
                 t_rf['mean_test_' + metric + '_perdif'] = 100 * (t_rf['mean_test_' + metric] - o_rf[
@@ -90,6 +98,10 @@ for t_folder in transf_folders:
             t_logr.to_csv(f'{transf_folder}/{t_folder}/{t_folder}_logr_avg.csv', sep='\t', index=False)
             t_nn.to_csv(f'{transf_folder}/{t_folder}/{t_folder}_nn_avg.csv', sep='\t', index=False)
 
+
+# concatenate the 5 iterations of original datasets
+for folder in org_folders:
+    concatenate(0, orig_folder, folder)
 
 # remove csv files
 # _, transf_folders, _ = next(walk(transf_folder))
